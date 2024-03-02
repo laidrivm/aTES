1de2ab3857d432689f5ad5223a8ff3b409ae5509
Vladimir <laidrivm@gmail.com>
2024-03-02 17:45:29 +0100

    Infra (#1)
    * init auth node.js app and docker
    
    * Stop tracking .DS_Store files
    
    * add task service and compose for it
    
    * add kafka to docker compose

---
+++ auth/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const http = require('node:http');
+//const fs = require("fs");
+//const bodyParser = require('body-parser');
+//const get = require('./routes/get');
+//const authorize = require('./routes/authorize');
+//const verify = require('./routes/verify');
+
+//require('dotenv').config();
+
+//if (!process.env.MONGO_CONNECTION) {
+//    console.error("MONGO_CONNECTION environment variable is not set.");
+//    process.exit(1);
+//}
+
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(express.static(__dirname + '/static'));
+
+//app.get('/', get);
+//app.post('/authorize', authorize);
+//app.post('/verify', verify);
+
+//const privateKey = fs.readFileSync( 'private.key' );
+//const certificate = fs.readFileSync( 'SSL.crt' );
+const server_config = {
+	//key : privateKey,
+	//cert: certificate
+};
+const port = 3001;
+const server = http.createServer(server_config, app);
+
+server.listen(port, (err) => {
+	console.log(`Node.js Express Server running on ${port}/`);
+});

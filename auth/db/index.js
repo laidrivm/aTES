97a68bd30135072c23f7d862e1f7a67d7569c9a1
Vladimir Lazarev <laidrivm@Vladimirs-MacBook-Pro.local>
2024-03-02 23:34:27 +0100

    fix an issue in auth service and mongo connection

--- auth/db/index.js
+++ auth/db/index.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 
 exports.connect = (app) => {
   const options = {
-    useNewUrlParser: true,
     autoIndex: false, // Don't build indexes
-      maxPoolSize: 10, // Maintain up to 10 socket connections
+    maxPoolSize: 10 // Maintain up to 10 socket connections
   };
 
   const connectWithRetry = () => {

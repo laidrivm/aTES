12f451e0f86856ef1eeaff3c9bf1f53d15464bf1
Vladimir Lazarev <laidrivm@Vladimirs-MacBook-Pro.local>
2024-03-10 13:16:40 +0100

    handle task assign and task closed events at accounting

--- accounting/db/consume.js
+++ accounting/db/consume.js
@@ -17,46 +17,83 @@ const consume = (consumer) => {
       const key = message.key.toString();
       const value = JSON.parse(message.value.toString());
 
-      switch (key) {
-        case 'user.created':
-          const user = new User({
-            user_id: value.data.user_id,
-            role: value.data.user_role,
-            balance: 0
-          });
-          await user.save();
+      if (key==='user.created') {
+        const user = new User({
+          user_id: value.data.user_id,
+          role: value.data.user_role,
+          balance: 0
+        });
+        await user.save();
+      }
 
-          break;
-        case 'user.updated':
-          break;
-        case 'task.created':
-          const task = new Task({
-            task_id: value.data.task_id,
-            description: value.data.task_description,
-            assignee: value.data.task_assignee,
-            assigned_price: value.data.assigned_price,
-            completed_price: value.data.completed_price
-          });
-          await task.save();
+      if (key==='user.updated'){
 
-          const transaction = new Transaction({
-            account_id: value.data.task_assignee,
-            task_id: value.data.task_id,
-            type: 'credit',
-            amount: value.data.assigned_price
-          });
-          await transaction.save();
+      }
 
-          await User.findOneAndUpdate(
-            { user_id: value.data.task_assignee },
-            { $inc: { balance: -value.data.assigned_price } }
-          );
+      if (key==='task.created'){
+        const task = new Task({
+          task_id: value.data.task_id,
+          description: value.data.task_description,
+          assignee: value.data.task_assignee,
+          assigned_price: value.data.assigned_price,
+          completed_price: value.data.completed_price
+        });
+        await task.save();
 
-          break;
-        case 'task.closed':
-          break;
-        case 'task.assigned':
-          break;
+        const transaction = new Transaction({
+          account_id: value.data.task_assignee,
+          task_id: value.data.task_id,
+          type: 'credit',
+          amount: value.data.assigned_price
+        });
+        await transaction.save();
+
+        await User.findOneAndUpdate(
+          { user_id: value.data.task_assignee },
+          { $inc: { balance: -value.data.assigned_price } }
+        );
+      }
+          
+      if (key==='task.closed') {
+        const task = await Task.findOneAndUpdate(
+          { task_id: value.data.task_id },
+          { status: "closed" },
+          { new: true }
+        );
+
+        const transaction = new Transaction({
+          account_id: task.assignee,
+          task_id: task.task_id,
+          type: 'debit',
+          amount: task.completed_price
+        });
+        await transaction.save();
+
+        await User.findOneAndUpdate(
+          { user_id: task.assignee },
+          { $inc: { balance: task.completed_price } }
+        );
+      }
+
+      if (key==='task.assigned') {
+        const task = await Task.findOneAndUpdate(
+          { task_id: value.data.task_id },
+          { assignee: value.data.task_assignee },
+          { new: true }
+        );
+
+        const transaction = new Transaction({
+          account_id: task.assignee,
+          task_id: task.task_id,
+          type: 'credit',
+          amount: task.assigned_price
+        });
+        await transaction.save();
+
+        await User.findOneAndUpdate(
+          { user_id: task.assignee },
+          { $inc: { balance: -task.assigned_price } }
+        );
       }
     }
   });
